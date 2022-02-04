6b69f1e325786a651ff8098b3fb9d44823d66a72
puremoru <e155745@ie.u-ryukyu.ac.jp>
2022-02-04 09:55:22 +0900

    :+1: MockServerComponentの実装

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Counter } from "./features/counter/Counter";
 import "./App.css";
 import RenderInput from "./RenderInput";
 import FrameworkList from "./FrameworkList";
+import MockServer from "./MockServer";
 
 function App() {
   const output = (text: string): void => {
@@ -32,6 +33,7 @@ function App() {
         <Counter />
         <RenderInput outputConsole={output} />
         <FrameworkList frameworks={data} />
+        <MockServer />
         {/* <FrameworkList /> */}
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
---
+++ src/MockServer.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const MockServer: React.VFC = () => {
+  const [clicked, setClicked] = useState<boolean>(false);
+  const [username, setUsername] = useState<string>("");
+  const [error, setError] = useState<string>("");
+
+  const fetchUser = async () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users/11")
+      .then((res) => {
+        const { username } = res.data;
+        setUsername(username);
+        setClicked(true);
+      })
+      .catch(() => {
+        setError("Fetching Failed !");
+      });
+  };
+
+  const buttonText = clicked ? "Loaded" : "Start Fetch";
+
+  return (
+    <div>
+      <button onClick={fetchUser} disabled={clicked}>
+        {buttonText}
+      </button>
+      {username && <h3>{username}</h3>}
+      {error && <p data-testid="error">{error}</p>}
+    </div>
+  );
+};
+
+export default MockServer;
